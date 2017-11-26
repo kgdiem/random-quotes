15c94e52040938a787c2826d57bcc9d916042201
Kevin <kevin@Kevins-MacBook-Pro.local>
2017-11-26 03:44:58 -0500

    add toggle loading test

--- resources/js/__tests__/toggleLoadingTest.js
+++ resources/js/__tests__/toggleLoadingTest.js
@@ -41,3 +41,18 @@ test('toggleLoading hides quote div and shows loading div on third call', () => 
   expect(document.getElementById('loading').style.display).toEqual('block');
   expect(document.getElementById('quote').style.display).toEqual("none");
 });
+
+test('toggleLoading hides loading div when loading div css is display, block', () => {
+  document.body.innerHTML = `
+    <div id="quote"></div>
+    <div id="loading"></div>`;
+
+  document.getElementById('loading').style.display = 'block';
+
+  const toggleLoading = require('../toggleLoading');
+
+  toggleLoading();
+
+  expect(document.getElementById('loading').style.display).toEqual('none');
+  expect(document.getElementById('quote').style.display).toEqual("block");
+});
