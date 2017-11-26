61339363ad7c2d3189769298d2db1ccaea06c280
Kevin <kevin@Kevins-MacBook-Pro.local>
2017-11-26 04:01:22 -0500

    add test for index.js

--- resources/js/__tests__/indexTest.js
+++ resources/js/__tests__/indexTest.js
@@ -1,1 +1,12 @@
+test('appendQuoteAndImage should be called when index is included', () => {
+  document.body.innerHTML = `<button></button>`;
 
+  const spy = jest.fn(() => 1);
+
+  const appendQuoteAndImage = jest.mock('../appendQuoteAndImage', () => spy);
+
+  require('../index.js');
+
+  expect(spy).toBeCalled();
+ 
+});
