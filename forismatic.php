3c028a2334a949e7f7524b1f6f172a518fba3d22
Kevin <kevin@Kevins-MacBook-Pro.local>
2017-11-26 07:11:27 -0500

    update php to check for invalid JSON from forismatic

--- forismatic.php
+++ forismatic.php
@@ -1,19 +1,26 @@
 <?php
+header('Access-Control-Allow-Origin: *');
 
 $baseURL = "https://api.forismatic.com/api/1.0/?";
 
 $requestURL = $baseURL . $_SERVER['QUERY_STRING'];
 
-$ch = curl_init(); 
+function getQuote($requestURL){
+  $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, $requestURL); 
+  curl_setopt($ch, CURLOPT_URL, $requestURL);
 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-$json = curl_exec($ch); 
+  $json = curl_exec($ch);
 
-curl_close($ch);
+  curl_close($ch);
 
-echo $json;
+  $json = json_encode(json_decode($json));
+
+  return (is_null($json) || $json === "null") ? getQuote($requestURL) : $json;
+}
+
+echo getQuote($requestURL);
 
 ?>
