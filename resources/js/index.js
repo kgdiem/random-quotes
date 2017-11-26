a3f8d5cc2406ac950d714e9f272b82b284cf3d0a
Kevin <kevin@Kevins-MacBook-Pro.local>
2017-11-26 01:18:23 -0500

    add method signatures

---
+++ resources/js/index.js
@@ -0,0 +1,37 @@
+appendQuoteAndImage();
+
+document.getElementsByTagName('button')[0].onclick = appendQuoteAndImage;
+
+async function appendQuoteAndImage(){
+  toggleLoading();
+
+  const quote = await getQuote();
+
+  appendQuote(quote);
+
+  const imageURL = await getImage();
+
+  appendImage(imageURL);
+
+  toggleLoading();
+}
+
+function toggleLoading(){
+
+}
+
+function getQuote(){
+
+}
+
+function appendQuote(){
+
+}
+
+function getImage(){
+
+}
+
+function appendImage(){
+
+}
