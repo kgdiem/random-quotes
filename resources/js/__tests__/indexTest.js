17a6d480297f66aad9cae2f8db98b0aad1f459c8
Kevin <kevin@Kevins-MacBook-Pro.local>
2017-11-26 01:42:28 -0500

    second failing test for showLoading

--- resources/js/__tests__/indexTest.js
+++ resources/js/__tests__/indexTest.js
@@ -11,3 +11,18 @@ test('toggleLoading hides the quote div and shows the loading div on first call'
   expect(document.getElementById('quote').style.display).toEqual("none");
 
 });
+
+test('toggleLoading hides loading div and shows quote div on second call', () => {
+  document.body.innerHTML = `
+    <div id="quote"></div>
+    <div id="loading"></div>`;
+
+  const toggleLoading = require('../toggleLoading');
+
+  toggleLoading();
+  toggleLoading();
+  
+  expect(document.getElementById('loading').style.display).toEqual('none');
+  expect(document.getElementById('quote').style.display).toEqual("block");
+
+});
