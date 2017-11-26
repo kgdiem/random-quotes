db716d03779af3a2a47844cffa7cf995cbb4475e
Kevin <kevin@Kevins-MacBook-Pro.local>
2017-11-26 01:49:05 -0500

    add toggleLoading

--- resources/js/toggleLoading.js
+++ resources/js/toggleLoading.js
@@ -1,4 +1,15 @@
 module.exports = function toggleLoading(){
+  const loadingDiv = document.getElementById('loading');
+  const quoteDiv = document.getElementById('quote');
+
+  if(loadingDiv.style.display === '' || loadingDiv.style.display === 'none'){
+    loadingDiv.style.display = 'block';
+    quoteDiv.style.display = 'none';
+  }
+  else if(loadingDiv.style.display === 'block'){
+    loadingDiv.style.display = 'none';
+    quoteDiv.style.display = 'block';
+  }
 
 }
 
