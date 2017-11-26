b5a781915febc61b8c730d280a915f0f9dffa23f
Kevin <kevin@Kevins-MacBook-Pro.local>
2017-11-26 02:53:04 -0500

    add code for getImage

--- resources/js/getImage.js
+++ resources/js/getImage.js
@@ -1,3 +1,7 @@
-module.exports = function getImage(){
+module.exports = function getImage(queryString){
+  const formattedQuery = queryString.replace(/ /g, '_');
 
+  return fetch(`https://api.giphy.com/v1/gifs/search?q=${formattedQuery}&api_key=inPBzYRrnBFaRTyTrSSRpD21B4HEO5xT`)
+    .then(res => res.json())
+    .then(json => Promise.resolve({url: json.data[0].images.downsized_medium}));
 }
