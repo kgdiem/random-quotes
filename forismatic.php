2070d6e070e62023797c7c2d4d78213fa2c5601f
Kevin <kevin@Kevins-MacBook-Pro.local>
2017-11-26 05:43:10 -0500

    add forismatic API passthrough

---
+++ forismatic.php
@@ -0,0 +1,19 @@
+<?php
+
+$baseURL = "https://api.forismatic.com/api/1.0/";
+
+$requestURL = $baseURL . $_SERVER['QUERY_STRING'];
+
+$ch = curl_init(); 
+
+curl_setopt($ch, CURLOPT_URL, $requestURL); 
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+$json = curl_exec($ch); 
+
+curl_close($ch);
+
+echo $json;
+
+?>
