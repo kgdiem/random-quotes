5c9fab8d18d830d512e85f418d312feb4432776c
Kevin <kevin@Kevins-MBP.fios-router.home>
2017-12-03 02:48:15 -0500

    update toggle loading test

--- resources/js/__tests__/toggleLoadingTest.js
+++ resources/js/__tests__/toggleLoadingTest.js
@@ -1,21 +1,25 @@
 test('toggleLoading hides the quote div and shows the loading div on first call', () => {
   document.body.innerHTML = `
-    <div id="quote"></div>
-    <div id="loading"></div>`;
+    <div id="quote">
+      <div id="content"></div>
+      <div id="loading"></div>
+    </div>`;
 
   const toggleLoading = require('../toggleLoading');
 
   toggleLoading();
 
   expect(document.getElementById('loading').style.display).toEqual('block');
-  expect(document.getElementById('quote').style.display).toEqual("none");
+  expect(document.getElementById('content').style.display).toEqual("none");
 
 });
 
 test('toggleLoading hides loading div and shows quote div on second call', () => {
   document.body.innerHTML = `
-    <div id="quote"></div>
-    <div id="loading"></div>`;
+    <div id="quote">
+      <div id="content"></div>
+      <div id="loading"></div>
+    </div>`;
 
   const toggleLoading = require('../toggleLoading');
 
@@ -23,14 +27,16 @@ test('toggleLoading hides loading div and shows quote div on second call', () =>
   toggleLoading();
   
   expect(document.getElementById('loading').style.display).toEqual('none');
-  expect(document.getElementById('quote').style.display).toEqual("block");
+  expect(document.getElementById('content').style.display).toEqual("block");
 
 });
 
 test('toggleLoading hides quote div and shows loading div on third call', () => {
   document.body.innerHTML = `
-    <div id="quote"></div>
-    <div id="loading"></div>`;
+    <div id="quote">
+      <div id="content"></div>
+      <div id="loading"></div>
+    </div>`;
 
   const toggleLoading = require('../toggleLoading');
 
@@ -39,13 +45,15 @@ test('toggleLoading hides quote div and shows loading div on third call', () => 
   toggleLoading();
 
   expect(document.getElementById('loading').style.display).toEqual('block');
-  expect(document.getElementById('quote').style.display).toEqual("none");
+  expect(document.getElementById('content').style.display).toEqual("none");
 });
 
 test('toggleLoading hides loading div when loading div css is display, block', () => {
   document.body.innerHTML = `
-    <div id="quote"></div>
-    <div id="loading"></div>`;
+    <div id="quote">
+      <div id="content"></div>
+      <div id="loading"></div>
+    </div>`;
 
   document.getElementById('loading').style.display = 'block';
 
@@ -54,5 +62,5 @@ test('toggleLoading hides loading div when loading div css is display, block', (
   toggleLoading();
 
   expect(document.getElementById('loading').style.display).toEqual('none');
-  expect(document.getElementById('quote').style.display).toEqual("block");
+  expect(document.getElementById('content').style.display).toEqual("block");
 });
