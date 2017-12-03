52e97bc998702b0b24edd30243529f6eb60ecae6
Kevin <kevin@Kevins-MBP.fios-router.home>
2017-12-03 02:28:47 -0500

    update js

--- resources/js/toggleLoading.js
+++ resources/js/toggleLoading.js
@@ -1,14 +1,14 @@
 module.exports = function toggleLoading(){
   const loadingDiv = document.getElementById('loading');
-  const quoteDiv = document.getElementById('quote');
+  const contentDiv = document.getElementById('content');
 
   if(loadingDiv.style.display === '' || loadingDiv.style.display === 'none'){
     loadingDiv.style.display = 'block';
-    quoteDiv.style.display = 'none';
+    contentDiv.style.display = 'none';
   }
   else if(loadingDiv.style.display === 'block'){
     loadingDiv.style.display = 'none';
-    quoteDiv.style.display = 'block';
+    contentDiv.style.display = 'block';
   }
 
 }
